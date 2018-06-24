62882a3900ee8319a9b634c1024660ec29728829
Cortland Walker <cortland1568@gmail.com>
2018-06-24 17:35:38 -0500

    Implemented Dummy piece that checks for empty tiles.

--- Boaz/Dummy.swift
+++ Boaz/Dummy.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2018 Cortland Walker. All rights reserved.
 //
 
-import Foundation
+import  UIKit
+
+class Dummy: Piece {
+    
+    // Stores x
+    private var xStorage: CGFloat!
+    // Stores y
+    private var yStorage: CGFloat!
+    
+    var x: CGFloat {
+        get {
+            return self.xStorage
+        }
+        set {
+            self.xStorage = newValue
+        }
+    }
+    var y: CGFloat {
+        get{
+            return self.yStorage
+        }
+        set {
+            self.yStorage = newValue
+        }
+    }
+    
+    init(frame: CGRect) {
+        self.xStorage = frame.origin.x
+        self.yStorage = frame.origin.y
+    }
+    
+    init() {
+        
+    }
+    
+}
